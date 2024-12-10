90c5136e50ee305730cda8f4d981767b274ef4be
thivagarradhakrishnan <ajithivagar@gmail.com>
2024-12-10 16:02:13 +0530

    hii

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import blogRoutes from './routes/blogRoutes.js';
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:3000','https://trafyblog-backend-255821839155.us-central1.run.app'], // Allow your frontend origin
+    origin: ['http://localhost:3000','https://blog.trafy.ai'], // Allow your frontend origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
     credentials: true
   }));
